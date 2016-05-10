7d6628ef86173e98f90b5f1368f443f0d5783011
vde <veronique.deslandres@univ-lyon1.fr>
2016-05-10 11:50:43 +0200

    Suppression commentaire STEP1 pour erreur Runsphere

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -5,27 +5,27 @@ class Sphere(object):
     def __init__(self, radius):
         # *** STEP 1 ***
         # uncomment the following line
-        #  self.radius = radius
+        self.radius = radius
 
     # *** STEP 2 ***
     # uncomment the 2 following lines
     #def __str__(self):
-    #    return '%s(%s)' % (self.__class__.__name__, self.radius)
+        #return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
         # *** STEP 3 ***
         # uncomment the following line
-        # return 4.0 * 3.1416 * self.radius ** 2
+        return 4.0 * 3.1416 * self.radius ** 2
 
     def volume(self):
         # *** STEP 4 ***
         # uncomment the following line
-        # return 4/3 * 3.1416 * self.radius ** 3
+        return 4/3 * 3.1416 * self.radius ** 3
 
     def diameter(self):
         # *** STEP 5 ***
         # uncomment the following line
-        #return self.radius
+        return self.radius
         pass
 
     def dump(self, filename):
