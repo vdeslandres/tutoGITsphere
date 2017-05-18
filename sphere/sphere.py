af141db08a538e7044fda6b0b5d4ff922c85a64f
veronique deslandres <veronique.deslandres@univ-lyon1.fr>
2017-05-18 09:42:36 +0200

    commentaire attendu remis

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -5,7 +5,7 @@ class Sphere(object):
     def __init__(self, radius):
         # *** STEP 1 ***
         # uncomment the following line
-        self.radius = radius
+        # self.radius = radius
 
     # *** STEP 2 ***
     # uncomment the 2 following lines
